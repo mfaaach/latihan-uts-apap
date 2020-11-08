5dbf4b40e1f3cb0a66b56849a68e0b2f331b3e3c
mfaaach <mfachri28@gmail.com>
2020-11-08 21:54:27 +0700

    init repo with application

---
+++ sifilm/src/main/java/apap/belajar/sifilm/SifilmApplication.java
@@ -0,0 +1,13 @@
+package apap.belajar.sifilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SifilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SifilmApplication.class, args);
+	}
+
+}
---
+++ sifilm/src/test/java/apap/belajar/sifilm/SifilmApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.belajar.sifilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SifilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
